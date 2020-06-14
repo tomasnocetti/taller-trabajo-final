4cc7297915d29dbea9d2294e791a1fd5ba720084
Tomas Nocetti <nocetti.tomas@gmail.com>
2020-06-14 13:08:11 -0300

    Remove comments

--- client/controllers/MapController.cpp
+++ client/controllers/MapController.cpp
@@ -17,11 +17,6 @@ MapController::~MapController() {
 
 void MapController::init(SdlWindow& window){
   MapData map = model.getMapData();
-  // THIS WILL GO SERVER SIDE
-  // ------
- // MapParser m;
-  //m.loadMap("client/assets/map/pindonga2.json");
-  //MapData& map = m.getMapData();
 
   for (unsigned int i = 0; i < map.tileSets.size(); i++){
     textures.push_back(new LTexture(window.createTexture()));
