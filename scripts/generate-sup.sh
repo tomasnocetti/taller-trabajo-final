927f8020cdee11ee134383dceb22b8ad846dc047
Fernando Fabbiano <ffabbiano@fi.uba.ar>
2020-06-18 18:43:43 -0300

    Supression files.

--- scripts/generate-sup.sh
+++ scripts/generate-sup.sh
@@ -6,4 +6,9 @@ valgrind \
 --suppressions=./scripts/supp/minimal-sdl2.supp \
 --suppressions=./scripts/supp/sdl2-texture.supp \
 --suppressions=./scripts/supp/minimal-sdl2-ttf-window.supp \
+--suppressions=./scripts/supp/ubuntu-desktop.supp \
+--suppressions=./scripts/supp/ubuntu-desktop2.supp \
+--suppressions=./scripts/supp/ubuntu-desktop3.supp \
+--suppressions=./scripts/supp/ubuntu-desktop4.supp \
+--suppressions=./scripts/supp/ubuntu-desktop5.supp \
 --log-file=minimalraw.log ./tpclient
