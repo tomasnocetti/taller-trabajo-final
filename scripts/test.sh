109849fa6933db6ffd8b1ed1ecf6e04b6dd51d32
Lautaro Fritz <lautarofritz2@gmail.com>
2020-06-19 19:09:42 -0300

    test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bash
-valgrind --leak-check=full --show-reachable=yes --error-limit=no --suppressions=./scripts/supp/minimal-sdl2.supp --suppressions=./scripts/supp/sdl2-texture.supp ./tp
+valgrind --leak-check=full --show-reachable=yes --error-limit=no --suppressions=./scripts/supp/minimal-sdl2.supp --suppressions=./scripts/supp/sdl2-texture.supp ./tpclient
 
